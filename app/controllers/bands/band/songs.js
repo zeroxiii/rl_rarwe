6f7a445dceabf1c2f2c2fcce1ceaf94f2c088565
Rahmaan Lodhia <rahmaan.lodhia@gmail.com>
2017-01-21 22:36:17 -0500

    Ch 8: Controllers - Smooth UI and flow

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,7 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  songCreationStarted: false,
+
+  isAddButtonDisabled: Ember.computed('title', function() {
+    return Ember.isEmpty(this.get('title'));
+  }),
+
+  canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  }),
+
   actions: {
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
+    },
+
     updateRating: function(params) {
       var song = params.item;
       var rating = params.rating;
